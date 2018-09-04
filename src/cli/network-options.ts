d48206d5c3e1a66e643d2edb25cf63f7f77ecb64
Luke Autry <lukeautry@gmail.com>
2018-09-04 12:27:34 -0400

    v1.0.13

--- src/cli/network-options.ts
+++ src/cli/network-options.ts
@@ -4,9 +4,9 @@ export interface INetworkOption {
 }
 
 export const networkOptions: INetworkOption[] = [
-  { label: 'Mainnet - Production', host: 'api.ercdex.com' },
-  { label: 'Kovan - Production', host: 'kovan-api.ercdex.com' },
-  { label: 'Mainnet - Staging', host: 'staging-api.ercdex.com' },
-  { label: 'Kovan - Staging', host: 'kovan-staging-api.ercdex.com' },
+  { label: 'Mainnet - Production', host: 'app.ercdex.com' },
+  { label: 'Kovan - Production', host: 'kovan.ercdex.com' },
+  { label: 'Mainnet - Staging', host: 'staging.ercdex.com' },
+  { label: 'Kovan - Staging', host: 'kovan-staging.ercdex.com' },
   { label: 'localhost (DEV)', host: 'localhost:8443' }
 ];
